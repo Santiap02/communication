482e8ab1be1a315266159fa373b2e7983846cc45
sanalv <sanalv@bancolombia.com.co>
2023-04-26 21:08:24 -0500

    Spring websocket security

---
+++ applications/app-service/src/main/java/co/com/bancolombia/config/MyCustomSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package co.com.bancolombia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class MyCustomSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/consumo/**")
+                        .permitAll()
+                )
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+}
--- applications/app-service/src/main/java/co/com/bancolombia/config/SecurityConfiguration.java
+++
@@ -1,8 +0,0 @@
-package co.com.bancolombia.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfiguration {
-
-}
